467e348cd24d637d04227d5b9f25f6aaa86827b2
imwrdo <sheinnickolas@gmail.com>
2024-12-23 21:59:41 +0100

    done

--- index.js
+++ index.js
@@ -18,15 +18,38 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+const isInvalidDate = (date) => {
+  return date.toString() === "Invalid Date";
+};
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", (req, res) => {
+  let date = new Date(req.params.date);
+  if(isInvalidDate(date)){
+    date = new Date(+req.params.date);
+  }
+  if(isInvalidDate(date)){
+    res.json({error: "Invalid Date"});
+  }
+
+  
+
+  res.json({
+    unix:date.getTime(),utc: date.toUTCString()
+  });
 });
 
-
+app.get("/api", (req, res) => {
+  let date = new Date();
+  res.json({
+    unix:date.getTime(),utc: date.toUTCString()
+  });
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+
+
